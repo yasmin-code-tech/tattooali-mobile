70c1bcee1ac0f22557979016d7997db3bacb06d1
Yasmin <yasmin.jobs33@gmail.com>
2026-02-04 19:42:28 -0300

    config: configuracao da estrutura de pastas do projeto

---
+++ tattooali/App.js
@@ -0,0 +1,26 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.texto} >Tela inicial de teste!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  texto: {
+    fontSize: 32,
+    color: 'red',
+
+  }
+});
